defbc88d37d16e4c366601de850da08aebe2f4c1
susanshrestha28 <51448029+susanshrestha28@users.noreply.github.com>
2021-03-22 22:06:11 +0545

    Betting

---
+++ index.js
@@ -0,0 +1,40 @@
+var express  = require('express');
+var app = express();
+var cors = require('cors');
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://football-prediction-api.p.rapidapi.com/api/v2/predictions");
+var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+var date = [year, month, day].join('-');
+console.log(date)
+req.query({
+	"iso_date": date,
+	"market": "classic",
+	"federation": "UEFA"
+});
+app.use(cors());
+
+app.get('', (reqs,ress) => {
+  console.log('client connected successfully');
+  req.headers({
+	"x-rapidapi-key": "d8ad551b48msh75b3c2b6967e1e9p19ea2ajsn5ea7ac0aa1b1",
+	"x-rapidapi-host": "football-prediction-api.p.rapidapi.com",
+	"useQueryString": true
+});
+req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+    ress.send(res.body);
+});
+})
+app.listen(3000, () => {
+    console.log('server listening at port 3000')
+})
